9cbaa744fdef934eef7c59b1ac5936b9ff9969ae
Corey Lutrick <coreylutrick@gmail.com>
2018-06-26 21:35:19 -0700

    added getting from firebase to display listings

--- src/app/App.js
+++ src/app/App.js
@@ -2,14 +2,35 @@ import React, { Component } from 'react';
 import Listings from '../components/Listings/Listings';
 import Building from '../components/Building/Building';
 import ListingForm from '../components/ListingForm/ListingForm';
+import connection from '../firebaseRequests/connection';
+import listingRequests from '../firebaseRequests/listings';
 import './App.css';
 
-class App extends Component {
-  render () {
+class App extends Component
+{
+  state =
+  {
+    listings: [],
+  };
+  componentDidMount ()
+  {
+    connection();
+    listingRequests.getRequest()
+      .then((listings) =>
+      {
+        this.setState({listings});
+      })
+      .catch((err) =>
+      {
+        console.error(err);
+      });
+  }
+  render ()
+  {
     return (
       <div className="App">
         <div className="col-sm-6">
-          <Listings />
+          <Listings listings={this.state.listings} />
         </div>
         <div className="col-sm-6">
           <Building />
--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -5,9 +5,19 @@ class Listings extends React.Component
 {
   render ()
   {
+    const {listings} = this.props;
+    const listingsItemComponents = listings.map((listing) =>
+    {
+      return (
+        <li key = {listing.id}>{listing.price}</li>
+      );
+    });
     return (
       <div className="Listings">
         <h2>Listings</h2>
+        <ul>
+          {listingsItemComponents}
+        </ul>
       </div>
     );
   }
